d46337b862a7c1331a4e994fa3b0969548944e5f
Opilane Techno+TLN <sergeihyvenen@gmail.com>
2026-09-22 17:52:12 +0300

    test setup complete

---
+++ TARge25Shop/SpaceShipTest/Macros/IMacros.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceShipTest.Macros
+{
+    public interface IMacros
+    {
+    }
+}
---
+++ TARge25Shop/SpaceShipTest/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TARge25Shop.ApplicationServices.Services;
+using TARge25Shop.Core.ServiceInterface;
+using TARge25Shop.Data;
+
+namespace SpaceShipTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+        }
+
+        public virtual void SetupServices(ServiceCollection services)
+        {
+            services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<TARge25ShopContext>(
+                x =>
+                {
+                    x.UseInMemoryDatabase("TEST");
+                    x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                }
+                )
+        }
+    }
+}
