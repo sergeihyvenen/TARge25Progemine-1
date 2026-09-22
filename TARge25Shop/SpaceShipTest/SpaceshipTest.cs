045c3f39fc64d35a3f700c7c6e8212bf46a82863
Opilane Techno+TLN <sergeihyvenen@gmail.com>
2026-09-22 19:30:17 +0300

    Added two more test

--- TARge25Shop/SpaceShipTest/SpaceshipTest.cs
+++ TARge25Shop/SpaceShipTest/SpaceshipTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TARge25Shop.Core.Domain;
 using TARge25Shop.Core.Dto;
 using TARge25Shop.Core.ServiceInterface;
 using Xunit;
@@ -9,61 +10,95 @@ namespace TARge25Shop.Spaceship.Test
 {
     public class SpaceshipTest : TestBase
     {
-
-        [Fact] //Fact tähistab ära ühte testi xUnit raamistijus
-        // 1 - Kirjeldatakse ära kas test on tavaline või negatiivne
-        // 2 - Kirjeldatakse ära mida parasjagu üritatakse testialuse objektiga teha
-        // 3 - Mis tingimusel tulemust kontrollitakse, peale tegevust
-        //
-        // Selles testis kontrollitakse et (2) Kosmolaeva lisamisel
-        // (1) Ei tohiks (3) saadud tulemus olla tühi. Jälgi seda sõnastuviisi
-        //
-        //           1                   2                       3
-        //           \/                  \/                      \/
+        [Fact]
         public async Task ShouldNot_AddEmptySpaceShip_WhenResultIsReturned()
         {
-            //Ülesseade
+            // Ülesseade
             SpaceshipDto dto = new SpaceshipDto()
             {
                 Name = "X AE a L 12 menuornvöerv",
                 ShipType = "lendav taldrik",
                 Crew = 67,
-                EnginePower = 69, //hobujõudu siis
+                EnginePower = 69,
                 CreatedAt = DateTime.Now,
                 UpdatedAt = DateTime.Now,
             };
 
-            //tegutsemine
+            // Tegutsemine
             var result = await Svc<ISpaceshipServices>().Create(dto);
 
-            //kontroll
+            // Kontroll
             Assert.NotNull(result);
         }
 
-        // 1 - Kirjeldatakse ära kas test on tavaline või negatiivne
-        // 2 - Kirjeldatakse ära mida parasjagu üritatakse testialuse objektiga teha
-        // 3 - Mis tingimusel tulemust kontrollitakse, peale tegevust
-        //
-        // Selles testis kontrollitakse et (2) Spaceshipi päring andmebaasist
-        // (1) Ei tohiks tagastada objekti (3) kui ID-d ei ole samad:
-        //
-        //           1                   2                       3
-        //           \/                  \/                      \/
-
         [Fact]
-
         public async Task ShouldNot_GetSpaceShipById_WhenIdNotEqual()
         {
-            // ülesseade
+            // Ülesseade
             Guid wrongGuid = Guid.NewGuid();
             Guid goodGuid = Guid.Parse("3be7da80-4ce7-452b-9809-ae9bbefb29f2");
 
-
-            // tegevus
+            // Tegevus
             await Svc<ISpaceshipServices>().DetailAsync(goodGuid);
 
-            // kontroll
+            // Kontroll
             Assert.NotEqual(wrongGuid, goodGuid);
+        }
+
+        [Fact]
+        public async Task Should_GetSpaceshipById_WhenGuidIsEqual()
+        {
+            // Ülesseade
+            Guid databaseGuid = Guid.Parse("3be7da80-4ce7-452b-9809-ae9bbefb29f2");
+            Guid seekGuid = Guid.Parse("3be7da80-4ce7-452b-9809-ae9bbefb29f2");
+
+            // Tegevus
+            await Svc<ISpaceshipServices>().DetailAsync(seekGuid);
+
+            // Kontroll
+            Assert.Equal(databaseGuid, seekGuid);
+        }
+
+        [Fact]
+        public async Task Should_SpaceshipDeletedById_WhenReturnedResultIsEqual()
+        {
+            // Ülesseade
+            SpaceshipDto dto = MockSpaceshupData();
+
+            // Tegevus
+            var addSpaceship = await Svc<ISpaceshipServices>().Create(dto);
+            var deleteSpaceship = await Svc<ISpaceshipServices>().Delete((Guid)addSpaceship.Id);
+
+            // Kontroll
+            Assert.Equal(addSpaceship.Id, deleteSpaceship.Id);
+        }
+
+        private SpaceshipDto MockSpaceshupData(bool isOneOrTwo = false)
+        {
+            if (isOneOrTwo == false)
+            {
+                return new SpaceshipDto
+                {
+                    Name = "X AE a L 12 menuornvöerv",
+                    ShipType = "lendav taldrik",
+                    Crew = 67,
+                    EnginePower = 69,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                };
+            }
+            else
+            {
+                return new SpaceshipDto
+                {
+                    Name = "RAKETT69",
+                    ShipType = "lendav kauss",
+                    Crew = 420,
+                    EnginePower = 69,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                };
+            }
         }
     }
 }
