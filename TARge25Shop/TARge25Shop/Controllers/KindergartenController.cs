6a8dcc4376d8ff87d3de8820ce28676d7df0778c
sergeihyvenen <sergeihyvenen@gmail.com>
2026-09-23 15:13:38 +0300

    Add Kindergarten view models and controller

---
+++ TARge25Shop/TARge25Shop/Controllers/KindergartenController.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARge25Shop.Core.Dto;
+using TARge25Shop.Core.ServiceInterface;
+using TARge25Shop.Data;
+using TARge25Shop.Models.Kindergarten;
+
+namespace TARge25Shop.Controllers
+{
+    public class KindergartenController : Controller
+    {
+        private readonly IKindergartenServices _kindergartenServices;
+        private readonly TARge25ShopContext _context;
+        public KindergartenController
+            (
+                IKindergartenServices kindergartenServices,
+                TARge25ShopContext context
+            )
+        {
+            _kindergartenServices = kindergartenServices;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Kindergartens
+                .Select(x => new KindergartenIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    ChildrenCount = x.ChildrenCount,
+                    KindergartenName = x.KindergartenName,
+                    TeacherName = x.TeacherName,
+                    CreatedAt = x.CreatedAt,
+                    UpdatedAt = x.UpdatedAt
+                });
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            KindergartenCreateUpdateViewModel result = new();
+
+            return View("CreateUpdate", result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            KindergartenCreateUpdateViewModel vm)
+        {
+            var dto = new KindergartenDto
+            {
+                GroupName = vm.GroupName,
+                ChildrenCount = vm.ChildrenCount,
+                KindergartenName = vm.KindergartenName,
+                TeacherName = vm.TeacherName
+            };
+
+            var result = await _kindergartenServices.Create(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var kindergarten =
+                await _kindergartenServices.DetailAsync(id);
+
+            if (kindergarten == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new KindergartenCreateUpdateViewModel
+            {
+                Id = kindergarten.Id,
+                GroupName = kindergarten.GroupName,
+                ChildrenCount = kindergarten.ChildrenCount,
+                KindergartenName = kindergarten.KindergartenName,
+                TeacherName = kindergarten.TeacherName,
+                CreatedAt = kindergarten.CreatedAt,
+                UpdatedAt = kindergarten.UpdatedAt
+            };
+
+            return View("CreateUpdate", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(
+            KindergartenCreateUpdateViewModel vm)
+        {
+            var dto = new KindergartenDto
+            {
+                Id = vm.Id,
+                GroupName = vm.GroupName,
+                ChildrenCount = vm.ChildrenCount,
+                KindergartenName = vm.KindergartenName,
+                TeacherName = vm.TeacherName,
+                CreatedAt = vm.CreatedAt,
+                UpdatedAt = vm.UpdatedAt
+            };
+
+            var result = await _kindergartenServices.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var kindergarten =
+                await _kindergartenServices.DetailAsync(id);
+
+            if (kindergarten == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new KindergartenDetailsViewModel
+            {
+                Id = kindergarten.Id,
+                GroupName = kindergarten.GroupName,
+                ChildrenCount = kindergarten.ChildrenCount,
+                KindergartenName = kindergarten.KindergartenName,
+                TeacherName = kindergarten.TeacherName,
+                CreatedAt = kindergarten.CreatedAt,
+                UpdatedAt = kindergarten.UpdatedAt
+            };
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var kindergarten =
+                await _kindergartenServices.DetailAsync(id);
+
+            if (kindergarten == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new KindergartenDeleteViewModel
+            {
+                Id = kindergarten.Id,
+                GroupName = kindergarten.GroupName,
+                ChildrenCount = kindergarten.ChildrenCount,
+                KindergartenName = kindergarten.KindergartenName,
+                TeacherName = kindergarten.TeacherName,
+                CreatedAt = kindergarten.CreatedAt,
+                UpdatedAt = kindergarten.UpdatedAt
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmation(Guid id)
+        {
+            var kindergarten =
+                await _kindergartenServices.Delete(id);
+
+            if (kindergarten == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TARge25Shop/TARge25Shop/Models/Kindergarten/KindergartenDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TARge25Shop.Models.Kindergarten
+{
+    public class KindergartenDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public int ChildrenCount { get; set; }
+        public string KindergartenName { get; set; } = string.Empty;
+        public string TeacherName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
