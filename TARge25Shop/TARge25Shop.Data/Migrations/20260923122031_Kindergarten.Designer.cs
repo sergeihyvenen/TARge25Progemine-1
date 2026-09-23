d69db868af787111ce7774b5daa0b56d34f072da
sergeihyvenen <sergeihyvenen@gmail.com>
2026-09-23 15:22:51 +0300

    Add Kindergarten views and database migration

--- TARge25Shop/TARge25Shop.Data/Migrations/20260923122031_Kindergarten.Designer.cs
+++ TARge25Shop/TARge25Shop.Data/Migrations/20260923122031_Kindergarten.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TARge25Shop.Data;
 
@@ -11,9 +12,11 @@ using TARge25Shop.Data;
 namespace TARge25Shop.Data.Migrations
 {
     [DbContext(typeof(TARge25ShopContext))]
-    partial class TARge25ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20260923122031_Kindergarten")]
+    partial class Kindergarten
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
